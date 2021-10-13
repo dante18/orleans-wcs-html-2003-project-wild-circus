275d126a474eb7b56379e160045bf3c4c2fea701
dante18 <lablandinerie2@gmail.com>
2021-10-13 14:51:52 +0200

    Adding the form manager

--- src/assets/js/modules/formManager/contactFormManager.js
+++ src/assets/js/modules/formManager/contactFormManager.js
@@ -4,7 +4,6 @@ import Form from '../classes/form/form'
 export const contactFormManager = {
   contactFormManager () {
     const form = new Form(Variables.contact.form.id, Variables.contact.form.inputCustomClass)
-    let numberFieldCompleted = 0
 
     /** Hide the previous notification following a previous send of the form **/
     window.setTimeout(() => {
@@ -18,23 +17,6 @@ export const contactFormManager = {
         document.querySelector(Variables.contact.form.notification.id).classList.add('d-none')
       }
     }, 3000)
-
-    /** Check if all fields are filled to activate the form button **/
-    form.getAllFieldForm().forEach((field) => {
-      field.addEventListener('change', (event) => {
-        if (form.fieldIsEmpty(event.target) > 0) {
-          numberFieldCompleted += 1
-        } else {
-          numberFieldCompleted -= 1
-        }
-
-        if (numberFieldCompleted === form.getAllFieldForm().length) {
-          document.querySelector(Variables.contact.form.btnFormId).disabled = false
-        } else {
-          document.querySelector(Variables.contact.form.btnFormId).disabled = true
-        }
-      })
-    })
 
     document.querySelector(Variables.contact.form.btnFormId).addEventListener('click', (event) => {
       event.preventDefault()
